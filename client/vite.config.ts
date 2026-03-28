74a8604854062df2c5e4ab9503829f64eaf287a1
Alexandre Quevedo <alexandrequevedo@MacBook-Pro-de-Alexandre.local>
2026-03-28 16:58:47 -0300

    chore: include package version in vite build and push fresh deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { execSync } from "child_process";
+import pkg from "./package.json";
 
 const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
@@ -9,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     __APP_BUILD_TIME__: JSON.stringify(new Date().toISOString()),
-    __APP_VERSION__: JSON.stringify(commitHash),
+    __APP_VERSION__: JSON.stringify(`${pkg.version} (${commitHash})`),
   },
   resolve: {
     alias: {
